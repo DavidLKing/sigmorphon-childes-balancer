ed9862cf80aeb79b7312d87783b3626775f4eb1b
David L. King <king.david.lee@gmail.com>
2018-09-20 15:48:49 -0400

    Adding wordform accuracy counter

---
+++ morphacc.py
@@ -0,0 +1,12 @@
+import sys
+
+correct = 0
+total = 0
+
+for l1, l2 in zip(open(sys.argv[1], 'r').readlines(), 
+                  open(sys.argv[2], 'r').readlines()):
+    if l1 == l2:
+        correct += 1
+    total += 1
+
+print("Accuracy:", correct / total)
